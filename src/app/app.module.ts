6e72e2f84dc797dd4a62adae5099b3d30304949c
Venusha Amarathinga <avenusha95@gmail.com>
2018-11-08 11:37:33 +0530

    full project 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { EnvironmentUrlService } from './shared/services/environment-url.service';
+import { HttpClientModule } from '@angular/common/http';
+import { RepositoryService } from './shared/services/repository.service'
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { NotFoundComponent } from './error-pages/not-found/not-found.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'owner', loadChildren: "./owner/owner.module#OwnerModule" },
+      { path: '404', component: NotFoundComponent },
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: '**', redirectTo: '/404', pathMatch: 'full' }
+    ])
   ],
-  providers: [],
+  providers: [
+    EnvironmentUrlService,
+    RepositoryService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
